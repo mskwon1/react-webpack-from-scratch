05660ccdc28d42b671d644015fd9e734204cc3ed
mskwon1 <mageeeeek@gmail.com>
2024-02-13 20:01:16 +0900

    Adds babel core & loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,18 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-  }, 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html')
