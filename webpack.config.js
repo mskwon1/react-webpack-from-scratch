859e405400ebfee8763bbe968a74af4c7500a488
mskwon1 <mageeeeek@gmail.com>
2024-02-13 20:03:29 +0900

    Adds bable presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+/**
+ * 
+ */
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
@@ -13,7 +16,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-        }
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
       },
     ]
   },
