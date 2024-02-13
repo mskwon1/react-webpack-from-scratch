7bec1ef7593e5d7c590e4992e537672bb82f7c63
mskwon1 <mageeeeek@gmail.com>
2024-02-13 20:44:26 +0900

    Adds App.jsx & moves index.html to public

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.?js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx']
+        },
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                { runtime: 'automatic' }
+              ]
+            ]
           }
         },
       },
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html')
+      template: path.join(__dirname, 'public', 'index.html')
     })
   ],
   mode,
+  devServer: {
+    port: 3000,
+    hot: true
+  }
 }
